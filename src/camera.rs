b7d636ac7928024ea724cbb1e1d260ba7002fcde
Ferdinand Mom <ferdinand.mom@epita.fr>
2022-02-28 13:12:07 +0100

    problem camera

--- src/camera.rs
+++ src/camera.rs
@@ -10,25 +10,22 @@ pub struct Camera {
     pub x_axis: Vector3<f32>,
     pub y_axis: Vector3<f32>,
     pub z_axis: Vector3<f32>,
-    pub top_left_start: Vector3<f32>,
     pub fov_x: f32, // horizontal field of view
-    pub focal_distance: f32,
+    pub top_left_start: Vector3<f32>,
 }
 
 impl Camera {
-    pub fn new(origin: Vector3<f32>, fov_x: f32, aspect_ratio: f32) -> Self {
+    pub fn new(origin_arg: Vector3<f32>, fov_x_arg: f32, aspect_ratio_arg: f32, focal_distance_arg: f32) -> Self {
 
-        let origin = origin;
-        let focal_distance = 1.0; 
-        let tmp = fov_x;
-        let fov_x = ((fov_x / 2.0) / 180.0) * PI; // radian
+        let fov_x = ((fov_x_arg * 0.5) / 180.0) * PI; // radian
+        let viewport_width = 2.0 * fov_x.tan() * focal_distance_arg;
+        let viewport_height = viewport_width / aspect_ratio_arg;
 
-        let viewport_width = 2.0 * fov_x.tan() * focal_distance;
-        let viewport_height = viewport_width / aspect_ratio;
+        let origin = origin_arg;
+        let x_axis = Vector3::new(viewport_width, 0.0, 0.0).normalize(); // right
+        let y_axis = Vector3::new(0.0, viewport_height, 0.0).normalize(); // up
+        let z_axis = x_axis.cross(&y_axis).normalize();  // depth
 
-        let x_axis = Vector3::new(viewport_width, 0.0, 0.0); // right
-        let y_axis = Vector3::new(0.0, viewport_height, 0.0); // up
-        let z_axis = Vector3::new(0.0, 0.0, focal_distance); // depth
         let top_left_start = origin - x_axis/2.0 + y_axis/2.0 + z_axis;
 
         Self {
@@ -36,9 +33,8 @@ impl Camera {
             x_axis,
             y_axis,
             z_axis,
-            top_left_start,
             fov_x,
-            focal_distance,
+            top_left_start,
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -15,32 +15,33 @@ use { crate::objects::*, crate::light::*, crate::scene::*, crate::camera::*, cra
 
 fn main() {
     let aspect_ratio = 16.0 / 9.0;
-    let canvas_width = 700_usize;
+    let canvas_width = 400_usize;
     let canvas_height = (canvas_width as f32 / aspect_ratio) as usize;
 
     let camera = Camera::new(
         Vector3::new(0.0, 0.0, 0.0),
-        125.0,
-        aspect_ratio
+        90.0,
+        aspect_ratio,
+        1.0
     );
 
     let mut scene = Scene::new(camera);
 
     scene.add_object(
         Box::new(Sphere {
-            center: Vector3::new(-1.25, 0.0, 3.0),
+            center: Vector3::new(0.0, 0.0, 10.0),
             radius: 0.5,
             textmat: Box::new(UniformTexture {})
         })
     );
     
-    scene.add_object(
-        Box::new(Sphere {
-            center: Vector3::new(0.0, 0.0, 2.0),
-            radius: 0.75,
-            textmat: Box::new(UniformTexture {})
-        })
-    );
+    // scene.add_object(
+    //     Box::new(Sphere {
+    //         center: Vector3::new(0.0, 0.0, 2.0),
+    //         radius: 0.75,
+    //         textmat: Box::new(UniformTexture {})
+    //     })
+    // );
 
     let mut pixels = vec![0; canvas_width * canvas_height * 3];
 
