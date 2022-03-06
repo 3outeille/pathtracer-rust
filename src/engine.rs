6634d7c7c33d87a80c4fd8e83b5ee4cfcc6578a2
ferdinand mom <ferdinandmom.business@gmail.com>
2022-03-06 08:56:04 +0100

    begin json + gui

--- src/engine.rs
+++ src/engine.rs
@@ -1,12 +1,143 @@
 
 use image::ColorType;
-use std::fs::File;
+use nalgebra::Vector3;
+use std::{fs::File, path::Path, f32::INFINITY, rc::Rc};
 use image::png::PNGEncoder;
-use {crate::scene::*, crate::utils::*, crate::ray::*};
+use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
+
+use crate::{camera::Camera, texture_material::UniformTexture, objects::{Sphere, Plane}, light::PointLight};
+
+use {crate::scene::*, crate::ray::*};
+
+
+#[derive(Serialize, Deserialize)]
+struct SomeDataType {}
+
 pub struct Engine;
 
 impl Engine {
     
+    pub fn parse_scene(&self, filename: &str) -> Result<Scene, std::io::Error> {
+        let json_file_path = Path::new(filename);
+        let json_file = File::open(json_file_path).expect("file not found");
+        let deserialized_camera: SomeDataType =
+            serde_json::from_reader(json_file).expect("error while reading json");
+        
+        let aspect_ratio = 16.0 / 9.0;
+        let up = Vector3::new(0.0, 1.0, 0.0);
+        let near_clipping_range = 0.5;
+        let far_clipping_range = INFINITY;    
+        let canvas_width = 1280_usize;
+        let canvas_height = (canvas_width as f32 / aspect_ratio) as usize;
+    
+        let camera = Camera::new(
+            Vector3::new(1.5, -0.1, -0.5),
+            Vector3::new(0.5, 0.0, 1.0),
+            up,
+            130.0,
+            near_clipping_range,
+            far_clipping_range,
+            aspect_ratio
+        );
+    
+        println!("{}x{}", canvas_width, canvas_height);
+        
+        let mut scene = Scene::new(camera, canvas_width, canvas_height);
+
+        let red  = Rc::new(
+            UniformTexture::new(
+                1.0,
+                1.5,
+                1.0,
+                15.0,
+                0.3,
+                Vector3::new(0.3, 0.1, 0.1)
+            )
+        );
+        
+        let green  = Rc::new(
+            UniformTexture::new(
+                1.0,
+                1.5,
+                1.0,
+                15.0,
+                0.3,
+                Vector3::new(0.1, 0.3, 0.1)
+            )
+        );
+
+        let blue = Rc::new(
+            UniformTexture::new(
+                1.0,
+                1.0,
+                1.0,
+                15.0,
+                0.3,
+                Vector3::new(0.3, 0.3, 0.8)
+            )
+        );
+
+        // Ground
+        // scene.add_object(
+        //     Rc::new(Plane {
+        //         center: Vector3::new(0.0, -6.0, 10.0),
+        //         normal: Vector3::new(0.0, 1.0, 0.0),
+        //         textmat: blue.clone()
+        //     })
+        // );
+
+        scene.add_object(
+            Rc::new(Sphere {
+                center: Vector3::new(0.0, -100.5, -1.0),
+                radius: 100.0,
+                textmat: blue.clone()
+            })
+        );
+
+        // Right-Background
+        scene.add_object(
+            Rc::new(Plane {
+                center: Vector3::new(0.0, 0.0, 50.0),
+                normal: Vector3::new(0.0, 0.0, -1.0),
+                textmat: blue.clone()
+            })
+        );
+
+        // Left-Background
+        scene.add_object(
+            Rc::new(Plane {
+                center: Vector3::new(-50.0, 0.0, 50.0),
+                normal: Vector3::new(1.0, 0.0, 0.0),
+                textmat: blue.clone()
+            })
+        );
+
+
+        scene.add_object(
+            Rc::new(Sphere {
+                center: Vector3::new(0.0, 0.0, 1.0),
+                radius: 0.5,
+                textmat: red.clone()
+            })
+        );
+
+        scene.add_object(
+            Rc::new(Sphere {
+                center: Vector3::new(1.0, 0.0, 1.0),
+                radius: 0.5,
+                textmat: green.clone()
+            })
+        );
+
+        scene.add_light(
+            PointLight::new(
+                Vector3::new(1.0, 1.0, 0.5),
+                0.9
+            )
+        );
+        return Ok(scene);
+    }
+
     pub fn render_scene(&self, scene: Scene) -> Vec<u8> {
         let mut pixels = vec![0; scene.canvas_width * scene.canvas_height * 3];
 
@@ -36,7 +167,14 @@ impl Engine {
     }
 
     pub fn render_scene_realtime(&self) {
-        todo!();
+        let app = app::App::default();
+        let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+        let mut frame = Frame::new(0, 0, 400, 200, "");
+        let mut but = Button::new(160, 210, 80, 40, "Click me!");
+        wind.end();
+        wind.show();
+        but.set_callback(move |_| frame.set_label("Hello World!")); // the closure capture is mutable borrow to our button
+        app.run().unwrap();
     }
 
     pub fn save_scene(&self, filename: &str, pixels: &[u8], width: usize, height: usize) -> Result<(), std::io::Error> {
--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,44 @@
 #![allow(unused_imports, unused_variables)]
 
-extern crate nalgebra;
-use std::{rc::Rc, f32::INFINITY};
-
+use std::{rc::Rc, f32::INFINITY, path::Path, fs::File};
 use nalgebra::Vector3;
+use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
+
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
 
 mod objects;
 mod light;
-mod utils;
 mod scene;
 mod camera;
 mod texture_material;
 mod ray;
 mod engine;
 
-use { crate::objects::*, crate::light::*, crate::scene::*, crate::camera::*, crate::texture_material::*, crate::utils::*, crate::ray::*, crate::engine::*};
+use { crate::objects::*, crate::light::*, crate::scene::*, crate::camera::*, crate::texture_material::*, crate::ray::*, crate::engine::*};
 
 fn main() {
     let aspect_ratio = 16.0 / 9.0;
-    let up = Vector3::new(0.0, 1.0, 0.0);
-    let near_clipping_range = 0.5;
-    let far_clipping_range = INFINITY;
     let canvas_width = 1280_usize;
     let canvas_height = (canvas_width as f32 / aspect_ratio) as usize;
 
-    let camera = Camera::new(
-        Vector3::new(1.5, -0.1, -0.5),
-        Vector3::new(0.5, 0.0, 1.0),
-        up,
-        130.0,
-        near_clipping_range,
-        far_clipping_range,
-        aspect_ratio
-    );
+    // Parse JSON
+    if let Ok(scene) = Engine.parse_scene("path/to/json") {
+        let pixels = Engine.render_scene(scene);
+        Engine.save_scene("output.png", &pixels, canvas_width, canvas_height).expect("error writing image");
+    }
 
-    println!("{}x{}", canvas_width, canvas_height);
+    // GUI
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+    let mut frame = Frame::new(0, 0, 400, 200, "");
+    let mut but = Button::new(160, 210, 80, 40, "Click me!");
+    wind.end();
+    wind.show();
+    but.set_callback(move |_| frame.set_label("Hello World!")); // the closure capture is mutable borrow to our button
+    app.run().unwrap();
 
-    let mut scene = Scene::new(camera, canvas_width, canvas_height);
-
-    let red  = Rc::new(
-        UniformTexture::new(
-            1.0,
-            1.5,
-            1.0,
-            15.0,
-            0.3,
-            Vector3::new(0.3, 0.1, 0.1)
-        )
-    );
-    
-    let green  = Rc::new(
-        UniformTexture::new(
-            1.0,
-            1.5,
-            1.0,
-            15.0,
-            0.3,
-            Vector3::new(0.1, 0.3, 0.1)
-        )
-    );
-
-    let blue = Rc::new(
-        UniformTexture::new(
-            1.0,
-            1.0,
-            1.0,
-            15.0,
-            0.3,
-            Vector3::new(0.3, 0.3, 0.8)
-        )
-    );
-
-    // Ground
-    // scene.add_object(
-    //     Rc::new(Plane {
-    //         center: Vector3::new(0.0, -6.0, 10.0),
-    //         normal: Vector3::new(0.0, 1.0, 0.0),
-    //         textmat: blue.clone()
-    //     })
-    // );
-
-    scene.add_object(
-        Rc::new(Sphere {
-            center: Vector3::new(0.0, -100.5, -1.0),
-            radius: 100.0,
-            textmat: blue.clone()
-        })
-    );
-
-    // Right-Background
-    scene.add_object(
-        Rc::new(Plane {
-            center: Vector3::new(0.0, 0.0, 50.0),
-            normal: Vector3::new(0.0, 0.0, -1.0),
-            textmat: blue.clone()
-        })
-    );
-
-    // Left-Background
-    scene.add_object(
-        Rc::new(Plane {
-            center: Vector3::new(-50.0, 0.0, 50.0),
-            normal: Vector3::new(1.0, 0.0, 0.0),
-            textmat: blue.clone()
-        })
-    );
-
-
-    scene.add_object(
-        Rc::new(Sphere {
-            center: Vector3::new(0.0, 0.0, 1.0),
-            radius: 0.5,
-            textmat: red.clone()
-        })
-    );
-
-    scene.add_object(
-        Rc::new(Sphere {
-            center: Vector3::new(1.0, 0.0, 1.0),
-            radius: 0.5,
-            textmat: green.clone()
-        })
-    );
-
-    scene.add_light(
-        PointLight::new(
-            Vector3::new(1.0, 1.0, 0.5),
-            0.9
-        )
-    );
-
-    let pixels = Engine.render_scene(scene);
-    Engine.save_scene("output.png", &pixels, canvas_width, canvas_height).expect("error writing image");
 }
