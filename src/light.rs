99d631a6d3240286078db0828800e2535412cf0a
Ferdinand Mom <ferdinand.mom@epita.fr>
2022-03-12 20:29:51 +0100

    clean light

--- src/light.rs
+++ src/light.rs
@@ -6,12 +6,3 @@ pub struct PointLight {
     pub position: Vector3<f32>,
     pub intensity: f32
 }
-
-impl PointLight {
-    pub fn new(position: Vector3<f32>, intensity: f32) -> Self {
-        Self {
-            position,
-            intensity
-        }
-    }
-}
